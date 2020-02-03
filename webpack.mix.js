978bb27194d5f5d447b2229f0965ba7572e5504e
Dev Kordes <devkordes@gmail.com>
2020-02-02 19:33:09 -0500

    Static pages cache

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,6 @@
 const mix = require('laravel-mix');
 
 mix
-  .webpackConfig({
-    resolve: {
-      alias: {
-        'react': 'preact/compat',
-        'react-dom': 'preact/compat',
-      },
-    },
-  })
   .setPublicPath('./')
   .copy('src/static', './public')
   .ts('src/ts/index.tsx', './public/js/app.js');
