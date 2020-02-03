c9688cf3fd3d1b6abeebf2314e85e279c886e6d0
Dev Kordes <devkordes@gmail.com>
2020-02-02 19:34:26 -0500

    Reverting webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,14 @@
 const mix = require('laravel-mix');
 
 mix
+  .webpackConfig({
+    resolve: {
+      alias: {
+        'react': 'preact/compat',
+        'react-dom': 'preact/compat',
+      },
+    },
+  })
   .setPublicPath('./')
   .copy('src/static', './public')
   .ts('src/ts/index.tsx', './public/js/app.js');
